472ea35753152f5a571e7e05aca860309b07cf8c
DADID <116240457+DADIDUNG@users.noreply.github.com>
2024-04-08 10:38:01 +0700

    Update alignedlayer_auto.sh

--- AlignedLayer/alignedlayer_auto.sh
+++ AlignedLayer/alignedlayer_auto.sh
@@ -12,16 +12,6 @@ fi
 sudo apt update && apt upgrade -y
 sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
 
-#Install GO
-ver="1.21.4"
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-go version
-
 cd $HOME
 wget https://github.com/yetanotherco/aligned_layer_tendermint/releases/download/v0.1.0/alignedlayerd
 chmod +x alignedlayerd
